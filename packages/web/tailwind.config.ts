2e06fbae64a9f5436d7832d2f74536be388ac95c
Hikotty <iseda.hikoto.dev@gmail.com>
2026-02-04 01:04:08 +0000

    feat: add Tremor library for dashboard UI

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    // Tremor module
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     container: {
       center: true,
